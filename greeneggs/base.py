fd1df645d36b792c0ff66ae3ce51df4295ce726a
samlikesphysics <sloomis@ucdavis.edu>
2022-02-15 21:17:43 -0800

    Added documentation site

---
+++ greeneggs/base.py
@@ -0,0 +1,12 @@
+def greetings(name):
+    """
+    Offers a colorful breakfast delicacy
+
+    :param name: Who am I? (I am who?)
+    :type name: str
+
+    :returns: None
+    """
+    print("I am %s!" % (name,))
+    print("%s I am!" % (name,))
+    print("Would you like green eggs and ham?")
